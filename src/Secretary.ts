8803ba62b6379e6763a1f50dcbfc2c41187e4346
Aaron Scherer <aequasi@gmail.com>
2019-03-20 22:25:50 -0700

    fix(release): Initial Release

--- src/Secretary.ts
+++ src/Secretary.ts
@@ -1,4 +1,5 @@
 import 'source-map-support/register';
+
 import {AbstractContextAdapter, AbstractPathAdapter, Context, PathResult, Result} from './Adapter';
 
 export default class Secretary<T extends AbstractPathAdapter | AbstractContextAdapter> {
